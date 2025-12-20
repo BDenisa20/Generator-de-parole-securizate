d7893b0bc8dee40cef909068421e54c3b42c5c30
BDenisa20 <elenadenisa.baloi@gmail.com>
2025-12-20 18:29:09 +0200

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -7,3 +7,22 @@ def load_common_password(file_path="common_pasword.txt"):
         return set(line.strip().lower() for line in f)
   except FileNotFoundError:
      return set()
+  
+COMMON_PASSWORD=load_common_password()
+
+def calculate_entrpy(password):
+   pool_size=0
+   if any(c in string.ascii_lowercase for c in password):
+      pool_size=pool_size+26
+   if any(c in string.ascii_uppercase for c in password):
+      pool_size=pool_size+26
+   if any(c in string.ascii_digits for c in password):
+      pool_size=pool_size+10
+   if any(c in string.punctuation for c in password):
+      pool_size=pool_size+len(string.punctuation)
+
+   if pool_size ==0:
+      return 0
+   
+   entropy=len(password)*math.log2(pool_size)
+   return round(entropy,2)
